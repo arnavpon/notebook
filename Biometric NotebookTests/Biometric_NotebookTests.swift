6911e6936010e98f0d71556bd289e416c35df1c4
Arnav Pondicherry <a_pondicherry@yahoo.com>
2015-12-25 10:10:00 -0500

    Initial Commit

---
+++ Biometric NotebookTests/Biometric_NotebookTests.swift
@@ -0,0 +1,36 @@
+//
+//  Biometric_NotebookTests.swift
+//  Biometric NotebookTests
+//
+//  Created by Arnav Pondicherry  on 12/25/15.
+//  Copyright © 2015 Confluent Ideals. All rights reserved.
+//
+
+import XCTest
+@testable import Biometric_Notebook
+
+class Biometric_NotebookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
